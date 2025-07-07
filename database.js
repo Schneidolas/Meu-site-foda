589385f712203febf5af7a7d5d2aa3489eb1659d
Schneidolas <109781303+Schneidolas@users.noreply.github.com>
2025-07-07 17:58:06 -0300

    Update database.js

--- database.js
+++ database.js
@@ -35,7 +35,7 @@ const youtubo_db = {
             thumbnail: 'https://i.ytimg.com/vi/5Cjrp23lBSM/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCELshUZ2cB9NTQQRypUloI954yUg',
             description: '',
             channelId: 'Flashdeck'
-        },
+        }
     },
 
     // ---- BANCO DE DADOS DE CANAIS (COM CUSTOMIZAÇÃO COMPLETA) ----
